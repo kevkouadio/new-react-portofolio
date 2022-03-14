ca2d08ded72e24298d1ffd56946a265c691c4358
kevkouadio <kevkouadio@gmail.com>
2022-03-14 19:39:00 -0400

    modification of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
-import'./pages.css'
+import { Button } from 'react-bootstrap';
+import Typist from 'react-typist';
+import './pages.css'
 
 function Home() {
 
     return (
-        <div className="home-container" >
-            <img src="https://kevkouadio.github.io/kk-portofolio/images/IMG-1808.jpg" width={200} />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
+        <div className='container-fluid' id="home-div">
+            <div className="home-container" > 
+                <img src="https://kevkouadio.github.io/kk-portofolio/images/IMG-1808.jpg" width={200} id="home-img"/>  
+                <br/>
+                <Typist>
+                    <h1>Hi! My name is Kouassi Kevin Kouadio,</h1>
+                    <h1>I'am a Full Stack Web Developper </h1>
+                    <h2>You can contact me by clicking on the Button below...</h2>
+                    <span><button className='btn btn-primary'>Contact Me</button></span>
+                </Typist>
+            </div>
         </div>
     )
 }
