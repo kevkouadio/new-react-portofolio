183b0dc994f46bdda514fb3d83cc488fd14e691c
kevkouadio <kevkouadio@gmail.com>
2022-03-25 00:14:28 -0400

    add of send email form on contact page

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,20 +2,21 @@ function Form() {
 
     return (
         <form id="contact-form">
-            <input type="hidden" name="contact_number" />
+            <h4 style={{color:"black"}}>Send me an email</h4>
             <div class="form-group">
-                <label for="exampleFormControlInput1">Name</label>
+                <label for="exampleFormControlInput1"></label>
                 <input type="text" name="user_name" class="form-control" id="exampleFormControlInput1" placeholder="Your Name"/>
             </div>
             <div class="form-group">
-                <label for="exampleFormControlInput1">Email address</label>
+                <label for="exampleFormControlInput1"></label>
                 <input type="email" name="user_email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
             </div>
             <div class="form-group">
-                <label for="exampleFormControlTextarea1">Message</label>
+                <label for="exampleFormControlTextarea1" style={{color:"black"}}>Message</label>
                 <textarea name="message" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-            <input class="btn btn-info" type="submit" value="Send" />
+            <br/>
+            <input class="btn btn-success" type="submit" value="Send" />
         </form>
     )
 }
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,35 +1,84 @@
-import Form from "../../components/Form"
+import { useState } from "react";
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 import "./contact.css"
 
 function Contact() {
+
+    const [isHidden, setIsHidden] = useState(true);
+    const [formObject, setFormObject] = useState({})
+
+    const msgSent = () => {
+        setIsHidden(false);
+    };
+    
+    const sendNewMsg = () => {
+        setIsHidden(true)
+    }
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({...formObject, [name]: value})
+      };
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_v8jpah3', 'template_1q5wguh', form.current, 'user_rdfOXx7Nnrnex29ZxAMhD')
+            .then((result) => {
+                console.log(result.text);
+                msgSent()
+            }, (error) => {
+                console.log(error.text);
+                alert("FAILED...", error);
+            });
+    };
+
 
     return (
         <div id="contact">
             <div className="container">
                 <h1>CONTACT ME</h1>
-                <p><i class="bi-telephone"></i> Phone: +1 862-339-8389</p>
-                <p><i class="bi-envelope"></i> Email: kevkouadio@gmail.com</p>
-                <p><i class="bi-github"></i> GitHub: <a href="https://github.com/kevkouadio/" target="blank">https://github.com/kevkouadio/</a></p>
-                <p><i class="bi-linkedin"></i> Linkdin: <a href="https://www.linkedin.com/in/kouassi-kevin-kouadio-b731a071/" target="blank">Kouassi Kevin KOUADIO</a></p>
-                
-                {/* <div className="card" id="contact-card">
-                    <div className="card-header">
-                        <h3>My Contact</h3>
+                <div className="row">
+                    <div className="col" id="contact-detail">
+                        <p><i class="bi-telephone"></i> Phone: +1 862-339-8389</p>
+                        <p><i class="bi-envelope"></i> Email: kevkouadio@gmail.com</p>
+                        <p><i class="bi-github"></i> GitHub: <a href="https://github.com/kevkouadio/" target="blank">https://github.com/kevkouadio/</a></p>
+                        <p><i class="bi-linkedin"></i> Linkdin: <a href="https://www.linkedin.com/in/kouassi-kevin-kouadio-b731a071/" target="blank">Kouassi Kevin KOUADIO</a></p>
                     </div>
-                    <div className="card-body">
-                        <div>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item"><i class="fa fa-phone" aria-hidden="true"></i> Phone: +1 862-339-8389</li>
-                                <li className="list-group-item"><i class="fa fa-github" aria-hidden="true"></i> GitHub: <a href="https://github.com/kevkouadio/" target="blank">https://github.com/kevkouadio/</a></li>
-                                <li className="list-group-item"><i class="fa fa-linkedin" aria-hidden="true"></i> Linkdin: <a href="https://www.linkedin.com/in/kouassi-kevin-kouadio-b731a071/" target="blank">Kouassi Kevin KOUADIO</a></li>
-                                <li className="list-group-item"><i class="fa fa-envelope" aria-hidden="true"></i> Email: kevkouadio@gmail.com</li>
-                            </ul>
+                    <div className="col">
+                        <div className={isHidden ? "form-card" : "form-card visually-hidden"}>
+                            <div className="card-body">
+                                <h2 className="p-2 border-bottom">Send me an email</h2>
+                                <form id="contact-form" ref={form} onSubmit={sendEmail}>
+                                    <div className="form-group">
+                                        <label for="exampleFormControlInput1">Name</label>
+                                        <input type="text" onChange={handleInputChange} name="user_name" className="form-control rounded-pill" id="exampleFormControlInput1" placeholder="Your Name" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="exampleFormControlInput1">Email address</label>
+                                        <input type="email" name="user_email" onChange={handleInputChange} className="form-control rounded-pill" id="exampleFormControlInput1" placeholder="name@example.com" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label for="exampleFormControlTextarea1">Message</label>
+                                        <textarea name="message" onChange={handleInputChange} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    </div>
+                                    <br />
+                                    <input className="btn btn-primary" type="submit" value="Send" disabled={!(formObject.user_name && formObject.user_email && formObject.message)} />
+                                </form>
+                            </div>
                         </div>
-                        <form action="mailto:kevkouadio@gmail.com" method="get" enctype="text/plain" target="blank">
-                            <input className="btn btn-primary" id="form-btn" type="submit" name="submit" value="Click here to send me an Email" />
-                        </form>
+                        <div class={isHidden ? "card visually-hidden": "card"} id="msgSentCard">
+                            <div class="card-body hidden">
+                                <h2 class="card-title text-success">Message Sent!</h2>
+                                <p class="card-text text-dark">Thanks for contacting me! I will be in touch with you shortly.</p>
+                                {/* <button class="btn btn-primary" onSubmit={sendNewMsg}>send a new message</button> */}
+                            </div>
+                        </div>
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     )
