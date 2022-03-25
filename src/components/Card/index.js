2ff63f3461f227e0e5f80b6aa21eeff9098af86f
kevkouadio <kevkouadio@gmail.com>
2022-03-25 11:16:00 -0400

    update of portofolio media queries

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 function Card(props) {
   return (
-    <div className="card" id="pCard" style={{width: 20+"rem" }}>
+    <div className="card" id="pCard" style={{width: 19+"rem" }}>
       {/* <h5 className="card-title">{props.name}</h5> */}
       {/* <i className="bi bi-arrows-angle-expand"></i> */}
         <img src={props.picture} className="card-img-top" alt={props.name}/>
